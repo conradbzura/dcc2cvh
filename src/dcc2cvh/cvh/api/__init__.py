d038ddeae25c9d4f7dbaa2b806d004fa86673239
Conrad <conradbzura@gmail.com>
2025-05-22 14:22:52 -0400

    Made database configurable via environment variables

--- src/dcc2cvh/cvh/api/__init__.py
+++ src/dcc2cvh/cvh/api/__init__.py
@@ -1,7 +1,10 @@
+import os
+from typing import Final
+
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
-DATABASE_URL = "mongodb://localhost:27017"
-DATABASE_NAME = "c2m2-backup"
-PAGE_SIZE = 25
+DATABASE_URL: Final = os.getenv("DATABASE_URL", "mongodb://0.0.0.0:27017")
+DATABASE_NAME: Final = os.getenv("DATABASE_NAME", "c2m2-backup")
+PAGE_SIZE: Final = 25
 
 db: AsyncIOMotorDatabase | None = None
